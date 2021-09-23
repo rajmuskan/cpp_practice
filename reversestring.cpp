17c0ed408bcf3f4fe2a2bcefddf3990ea02d9286
Muskan Raj <81063485+rajmuskan@users.noreply.github.com>
2021-09-23 16:41:40 +0530

    Add files via upload

---
+++ reversestring.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    string s;
+    cin>>s;
+    int i=0;
+    int j=s.length()-1;
+    while(i<j){
+        int temp=s[i];
+        s[i]=s[j];
+        s[j]=temp;
+        i++;
+        j--;
+    }
+
+    cout<<s;
+ 
+ return 0;
+}
